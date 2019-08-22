a746cf9043da61e89eb7f41a8a1dea1bd8c2e380
LeeTrent <lee.trent.1@gmail.com>
2019-08-21 22:08:53 -0500

    Adds fonts and images to project

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column (
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/lee.jpg'),
+              ),
+              Text(
+                'Lee Trent',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                )
+              ),
+              Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 2.5
+                  )
+              )
+            ],
+        ),
+          ),
+        ),
+      );
+  }
+}
+class yyyMyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
