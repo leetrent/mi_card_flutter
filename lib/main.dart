3447baf93e6ae4cd092c0bc69605778832e71235
LeeTrent <lee.trent.1@gmail.com>
2019-08-25 10:57:16 -0500

    Switches to using the Card widget for phone number

--- lib/main.dart
+++ lib/main.dart
@@ -37,38 +37,33 @@ class MyApp extends StatelessWidget {
                       letterSpacing: 2.5
                   )
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('+1-541-754-3010',
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+1-541-754-3010',
                     style: TextStyle(
                       color: Colors.teal.shade900,
                       fontFamily: 'SourceSansPro',
                       fontSize: 20.0,
-                    )),
-                  ],
+                      ),
+                  ),
                 ),
               ),
-              Container(
+              Card(
                 color: Colors.white,
                 margin: EdgeInsets.symmetric(
                   vertical: 10.0,
                   horizontal: 25.0,
                 ),
-                padding: EdgeInsets.all(10.0),
                 child: Row(
                   children: <Widget>[
                     Icon(
